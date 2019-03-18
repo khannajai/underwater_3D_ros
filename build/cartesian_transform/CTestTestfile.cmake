245dd1f51d38799b3088e2820d71fb41ee95ba2c
Jai Khanna <jai41khanna@gmail.com>
2019-03-18 12:20:18 +0100

    fix remove build dir

--- build/cartesian_transform/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jai/catkin_ws/src/cartesian_transform
-# Build directory: /home/jai/catkin_ws/build/cartesian_transform
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
